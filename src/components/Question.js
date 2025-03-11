a38257f4db5bb513c047fb5df4b61fb7a8e7189b
gireslaragon <giresnkonga9@gmail.com>
2025-03-11 21:24:47 +0100

    commit Onze

--- src/components/Question.js
+++ src/components/Question.js
@@ -308,7 +308,7 @@ function Question() {
     
     return (
         <div className="mt-28 lg:mt-28 bg-gradient-to-r from-Three from-20% via-Two via-30% to-Four to-90%">
-            <div className='flex flex-col gap-10 max-w-[1400px] mx-auto px-10 py-14 text-white'>
+            <div className='flex flex-col gap-10 max-w-[1400px] mx-auto px-4 py-14 text-white'>
                 <h2 className="text-white text-2xl lg:text-4xl font-medium">Vous avez des questions ?</h2>
 
                 <div>
